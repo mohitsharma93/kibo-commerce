20192894ea6e0032dbec3efd0a85a3bd1df559e0
Mohit Sharma <mohitjisharma07@gmail.com>
2025-07-12 14:47:08 +0530

    feat: update styles and improve header component
    - Updated CSS variables in styles.css to include new color definitions and spacing adjustments.
    - Added a new build script for production in package.json to run Playwright tests before building.
    - Re-enabled the local development server in playwright.config.ts for testing.
    - Refactored header.component.ts to remove unused imports and streamline the component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,15 @@
 import { Component, DestroyRef, inject } from '@angular/core';
 import { BehaviourService } from '../services/behaviour-service';
-import { CartItems } from '../types/products';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { map, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { elementIsVisibleInViewport } from '../utils';
 import { FilterInputComponent } from "../filter-input/filter-input.component";
 
 @Component({
   standalone: true,
   selector: 'app-header',
   templateUrl: './header.component.html',
-  imports: [CommonModule, FilterInputComponent],
+  imports: [CommonModule],
 })
 export class HeaderComponent {
 
